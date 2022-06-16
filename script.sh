0c4ec7b846eccb0600fbdc4c20f633b03c8c16c2
ThomasCPNV <107610215+ThomasCPNV@users.noreply.github.com>
2022-06-16 09:30:08 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ apt update -y
 apt install -y docker docker-compose git wget
 git clone https://github.com/7ric/Photoblog.git /Photoblog
 sed -i 's/localhost/db/g' /Photoblog/classes/db.php
-wget -nc https://raw.githubusercontent.com/ThomasCPNV/VIR2/docker-compose.yaml
+wget -nc https://raw.githubusercontent.com/ThomasCPNV/VIR2/main/docker-compose.yaml
 docker-compose up -d
 docker-compose exec -T websrv docker-php-ext-install mysql
 docker-compose exec -T websrv /etc/init.d/apache2 reload
