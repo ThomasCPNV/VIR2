338d0d7f4a87bf66f52fec277b08aebd419541df
ThomasCPNV <107610215+ThomasCPNV@users.noreply.github.com>
2022-06-16 11:38:19 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ sed -i 's/localhost/db/g' /Photoblog/classes/db.php
 wget -nc https://raw.githubusercontent.com/ThomasCPNV/VIR2/main/docker-compose.yaml
 docker-compose up -d
 docker-compose exec -T websrv docker-php-ext-install mysql
-docker-compose exec -T websrv /etc/init.d/apache2 restart
+docker-compose exec -T websrv /etc/init.d/apache2 reload
