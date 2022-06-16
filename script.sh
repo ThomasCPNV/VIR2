e4bf229e77bfd05ae035d7bf53de00068b2819aa
ThomasCPNV <107610215+ThomasCPNV@users.noreply.github.com>
2022-06-16 11:39:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt -y update
+apt -y update && upgrade
 apt install -y docker docker-compose git
 git clone https://github.com/7ric/Photoblog.git /Photoblog
 sed -i 's/localhost/db/g' /Photoblog/classes/db.php
